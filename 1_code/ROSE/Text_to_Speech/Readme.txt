f7f664d062840676924fba8e7f6f801f33c020ba
Srihari Chekuri <svc31@scarletmail.rutgers.edu>
2016-04-03 20:24:03 -0400

    added tts

---
+++ 1_code/ROSE/Text_to_Speech/Readme.txt
@@ -0,0 +1,4 @@
+Dependencies:
+Python 3
+pyttsx
+1. to run: python3 tts.py
---
+++ 1_code/ROSE/Text_to_Speech/tts.py
@@ -0,0 +1,13 @@
+import pyttsx
+
+engine = pyttsx.init()
+engine.setProperty('rate', 70)
+
+voices = engine.getProperty('voices')
+
+for voice in voices:
+	print "Using Voice" , repr(voice)
+	engine.setProperty('voice', voice.id)
+	engine.say("Please move out of the way")
+
+engine.runAndWait()
