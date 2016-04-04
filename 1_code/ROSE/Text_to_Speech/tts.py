aa486bd95c779c3b72c5d18002b7cc25156370b7
shree <svc31@scarletmail.rutgers.edu>
2016-04-03 20:57:53 -0400

    readme in tts

--- 1_code/ROSE/Text_to_Speech/tts.py
+++ 1_code/ROSE/Text_to_Speech/tts.py
@@ -8,6 +8,7 @@ voices = engine.getProperty('voices')
 for voice in voices:
 	print "Using Voice" , repr(voice)
 	engine.setProperty('voice', voice.id)
+	#Add your message below
 	engine.say("Please move out of the way")
 
 engine.runAndWait()
